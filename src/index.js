0bbdfaa5e7db8ded396eb29b517d927e295132a6
Alexander Rovang <DXRovang@gmail.com>
2023-03-27 22:11:23 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+// import drawReducer from './reducers/drawReducer'
+// import cardReducer from './reducers/cardReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './assets/styles.css';
+
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const combinedReducer = combineReducers(
+//   {draws: drawReducer, cards: cardReducer}
+// )
+
+// let store = createStore(combinedReducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <Provider store={store}> */}
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
+    {/* </Provider> */}
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
